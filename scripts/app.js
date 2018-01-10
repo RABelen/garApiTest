4558c44276c9424cf22c6143b25f62ac25057154
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2018-01-10 23:45:46 +0800

    Updated multiFetch view

--- scripts/app.js
+++ scripts/app.js
@@ -15,9 +15,9 @@ angular
     $scope.params = {
       check_in: today,
       check_out: tom,
-      nights: "",
+      nights: 1,
       cancellation_rules: 1,
-      rinfo: [[18, 18]],
+      rinfo: "[[18,18]]",
       trans_id: "67f6d937"
     };
 
@@ -40,6 +40,8 @@ angular
     $scope.fetch = function(req, rows) {
       let data = new FormData();
       $scope.results = [];
+      $scope.hotelsCount = 0;
+      $scope.roomsCount = 0;
 
       multiFetch(req, rows);
 
@@ -69,12 +71,20 @@ angular
       let fd = new FormData();
 
       angular.forEach(rows, function(row) {
-        fd.append("property_id[]", row.id);
+        if (row.id != "") {
+          $scope.hotelsCount++;
+          fd.append("property_id[]", row.id);
 
-        $scope.results.push({
-          hotelId: row.id,
-          hotelName: row.name
-        });
+          $scope.results.push({
+            hotelId: row.id,
+            hotelName: row.name,
+            rooms: [{
+              multi: {},
+              single: {},
+              prebook: {}
+            }]
+          });
+        }
       });
 
       let post = {
@@ -90,32 +100,33 @@ angular
           let x2js = new X2JS();
           let data = x2js.xml_str2json(res);
           let rooms = [];
-          
-          console.log(data)
+
+          console.log(data);
 
           angular.forEach(data["room-stays"]["room-stay"], function(room) {
+            $scope.roomsCount++;
+
             rooms.push({
               hotelId: room.room["hotel-id"],
               roomId: room.room["room-id"],
               title: room.room.title.__text,
+              url: room["landing-url"],
               ratePlanCode: room["rate-plan-code"],
-              multiRate: room["display-pricing"].total
+              displayPrice: room["display-pricing"].total,
+              bookingPrice: room["booking-pricing"].total
             });
-            console.log(room)
           });
 
           for (let hotel = 0; hotel < $scope.results.length; hotel++) {
-            $scope.results[hotel].rooms = [];
-
+            $scope.results[hotel].rooms.multi = [];
+            
             for (let room = 0; room < rooms.length; room++) {
               if (rooms[room].hotelId == $scope.results[hotel].hotelId) {
-                $scope.results[hotel].rooms.push(rooms[room]);
+                $scope.results[hotel].rooms.multi.push(rooms[room]);
+                singleFetch(req, rooms[room].hotelId);  
               }
             }
           }
-
-          console.log($scope.results.length);
-          console.log(rooms.length);
         })
         .catch(function(err) {
           let x2js = new X2JS();
@@ -131,23 +142,21 @@ angular
         row.id
       }/room_availability?check_in=${formatDate(
         req.check_in
-      )}&check_out=${formatDate(req.check_out)}&nights=${
-        req.nights
-      }&cancellation_rules=${req.cancellation_rules}&api_key=${
-        auth.api_key
-      }&auth_token=${auth.auth_token}&rinfo=${req.rinfo}&transaction_id=${
-        req.trans_id
-      }`;
+      )}&check_out=${formatDate(req.check_out)}&cancellation_rules=${
+        req.cancellation_rules
+      }&api_key=${auth.api_key}&auth_token=${auth.auth_token}&rinfo=${
+        req.rinfo
+      }&transaction_id=${req.trans_id}`;
 
       $http
         .get(singleUrl)
         .success(function(res) {
           let x2js = new X2JS();
           let data = x2js.xml_str2json(res);
-          let result = angular.extend({}, row, data);
+          // let result = angular.extend({}, row, data);
 
-          console.log(result);
-          return result;
+          console.log(data);
+          // return result;
         })
         .catch(function(err) {
           let x2js = new X2JS();
