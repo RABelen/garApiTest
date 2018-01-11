0d6df945b08c9cc1b47fbf27696a3161fad4281f
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2018-01-12 02:10:50 +0800

    Added styling

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ angular
     const base_url = "https://availability.integration2.testaroom.com";
     let today = new Date();
     let tom = new Date();
-    
+
     tom.setDate(tom.getDate() + 1);
 
     $scope.rows = [];
@@ -21,7 +21,7 @@ angular
       rinfo: "[[18,18]]",
       trans_id: "67f6d937"
     };
-    
+
     $scope.listProperties = function() {
       let url = `https://book.integration2.testaroom.com/api/properties.csv?api_key=${
         $scope.auth.api_key
@@ -58,12 +58,9 @@ angular
       $scope.hotelsCount = 0;
       $scope.multiCount = 0;
       $scope.singleCount = 0;
-
+      $scope.prebookCount = 0;
+      
       multiFetch(req, rows);
-      $scope.message = {
-        type: "Finished",
-        text: `Queried ${$scope.hotelsCount} hotels.`
-      };
     };
 
     let formatDate = function(date) {
@@ -75,9 +72,9 @@ angular
         req.check_in
       )}&check_out=${formatDate(req.check_out)}&cancellation_rules=${
         req.cancellation_rules
-      }&api_key=${$scope.auth.api_key}&auth_token=${$scope.auth.auth_token}&rinfo=${
-        req.rinfo
-      }&transaction_id=${req.trans_id}`;
+      }&api_key=${$scope.auth.api_key}&auth_token=${
+        $scope.auth.auth_token
+      }&rinfo=${req.rinfo}&transaction_id=${req.trans_id}`;
 
       let fd = new FormData();
 
@@ -107,14 +104,16 @@ angular
           let x2js = new X2JS();
           let data = x2js.xml_str2json(res);
           let rooms = [];
-          
+
           angular.forEach(data["room-stays"]["room-stay"], function(room) {
             $scope.multiCount++;
-
+            console.log(room)
             rooms.push({
               hotelId: room.room["hotel-id"],
               roomId: room.room["room-id"],
               title: room.room.title.__text,
+              description: room.room.description.__text,   
+              url: room["landing-url"],                       
               rates: [
                 {
                   ratePlanCode: room["rate-plan-code"],
@@ -146,9 +145,9 @@ angular
         req.check_in
       )}&check_out=${formatDate(req.check_out)}&cancellation_rules=${
         req.cancellation_rules
-      }&api_key=${$scope.auth.api_key}&auth_token=${$scope.auth.auth_token}&rinfo=${
-        req.rinfo
-      }&transaction_id=${req.trans_id}`;
+      }&api_key=${$scope.auth.api_key}&auth_token=${
+        $scope.auth.auth_token
+      }&rinfo=${req.rinfo}&transaction_id=${req.trans_id}`;
 
       $http
         .get(singleUrl, { timeout: 10000 })
@@ -157,8 +156,6 @@ angular
           let data = x2js.xml_str2json(res);
           let rooms = [];
 
-          console.log(`Running singleFetch ${new Date().getTime()}`);
-          
           angular.forEach(data["room-stays"]["room-stay"], function(room) {
             $scope.singleCount++;
 
@@ -178,6 +175,23 @@ angular
           });
 
           pushResult($scope.results, rooms);
+
+          angular.forEach(rooms, function(room) {
+            let codes = room.rates
+              .map(code => code.ratePlanCode)
+              .filter((value, index, self) => self.indexOf(value) === index);
+
+            angular.forEach(codes, function(code) {
+              $timeout(
+                prebookFetch(req, {
+                  hotel: room.hotelId,
+                  room: room.roomId,
+                  code: code
+                }),
+                10000
+              );
+            });
+          });
         })
         .catch(function(err) {
           $scope.message = {
@@ -187,14 +201,60 @@ angular
         });
     };
 
+    let prebookFetch = function(req, params) {
+      let prebookUrl = `${base_url}/api/1.1/properties/${
+        params.hotel
+      }/room_availability?check_in=${formatDate(
+        req.check_in
+      )}&check_out=${formatDate(req.check_out)}&cancellation_rules=${
+        req.cancellation_rules
+      }&room_id=${params.room}&rate_plan_code=${params.code}&api_key=${
+        $scope.auth.api_key
+      }&auth_token=${$scope.auth.auth_token}&rinfo=${
+        req.rinfo
+      }&transaction_id=${req.trans_id}`;
+
+      $http
+        .get(prebookUrl, { timeout: 10000 })
+        .success(function(res) {
+          let x2js = new X2JS();
+          let data = x2js.xml_str2json(res);
+          let rooms = [];
+          let room = data["room-stays"]["room-stay"];
+
+          $scope.prebookCount++;
+
+          rooms.push({
+            hotelId: room.room["hotel-id"],
+            roomId: room.room["room-id"],
+            title: room.room.title.__text,
+            url: room["landing-url"],
+            rates: [
+              {
+                ratePlanCode: room["rate-plan-code"],
+                displayPrice: room["display-pricing"].total,
+                requestType: "Pre Book"
+              }
+            ]
+          });
+
+          pushResult($scope.results, rooms);
+        })
+        .catch(function(err) {
+          $scope.message = { type: "Error", text: err.statusText };
+        });
+    };
+
     let pushResult = function(result, data) {
       for (let hotel = 0; hotel < result.length; hotel++) {
         for (let room = 0; room < data.length; room++) {
           if (data[room].hotelId == result[hotel].hotelId) {
-            let filter = _.find(result[hotel].rooms, { roomId: data[room].roomId})
+            let filter = _.find(result[hotel].rooms, {
+              roomId: data[room].roomId
+            });
             if (filter) {
               filter.rates.push(data[room].rates[0]);
-            } else {          
+            } else {
               result[hotel].rooms.push(data[room]);
             }
           }
